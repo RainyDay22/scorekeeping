256cfdc705468653d81f3c99a2ab7b0ed9ada65c
Ruining D <ruiningd@Ruinings-MacBook-Air-2.local>
2022-09-06 14:52:19 +0200

    Completed init_file function in score.cpp
    purpose:
    - the scorekeeping application relies on this file as memory to have permanence outside of runtime
    - existing files are able to be specified and new files created for continuous tracking of the same competition
    
    change in design choices:
    - all filename inputs are standardized instead of having a special character indicating a new file should be created
    - this avoids the problem of duplicates or of choosing an inconvenient/confusing special character
    
    testing:
    - run ./score and input "new_file.txt", the program creates the empty file
    - run the same command again, no new file is created and the programme prints that this file exists
    
    note:
    - it is up to the user to make sure the specified existing file has read and write privileges, the programme will not change that file metadata

--- score.cpp
+++ score.cpp
@@ -1,40 +1,102 @@
 #include <iostream>
 #include <string.h>
+#include <fstream>
+#include <map>
 
-
-int init_file(std::string filename){
-    //if starts with - then create new file 
-    //else, check if file exists
-        // if read/writeable return 1
-        // if no existence or wrong permissions 
-        return 0;
-}
+int init_file(std::string filename, std::fstream* file);
+int init_scbd(std::fstream* file, std::map<std::string,int>* scbd) {return 1;};
+void edit_ptcp() {};
+void edit_pts() {};
+void print_lboard() {};
+void save() {};
 
 int main(){
     //init vars
-    std::string f_name;
-    
-    std::cout<<"Hello World";
-    return 1;
+    std::string f_name; //filename
+    std::fstream* score_file; //scoreboard file
+    std::map<std::string,int> scbd; //real time scoreboard data structure
 
     // welcome message + list of possible actions
     std::cout << "Welcome to scorekeeper!";
 
     // require storage file name/creation => load/create
     while(true) {
-        std::cout << "Input storage filename or type '-Bob' to create a new file with name Bob ";
+        std::cout << "Input storage filename";
         std::cin>> f_name;
-        if (init_file(f_name)) 
+        if (init_file(f_name, score_file)) 
             break;
         std::cout << "Error. Try again.";
     }
 
-    // give rundowm of possible actions
+
+    if (!init_scbd(score_file, &scbd)){
+        std::cout<<"score_file load error";
+        return 0;
+    }
+
+/*
+    // give rundown of possible actions
     std::cout<<"Possible actions: participant+/-, points+/-, print, save";
 
     // for loop to process argument, read until endl
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+        std::cout << line << std::endl;
+
+        if(line=="exit") break;
+
+        char action ='o';
+        if(line.find("ptcp")==0) action='a';
+        else if(line.find("pts")==0) action='t';
+        else if(line.find("print")==0) action='r';
+        else if(line.find("save")==0) action='s';
+        else action='x';
+
+        switch(action){
+            case 'a':
+                edit_ptcp();
+                break;
+            case 't':
+                edit_pts();
+                break;
+            case 'r':
+                print_lboard();
+                break;
+            case 's':
+                save();
+                break;
+            default:    
+                std::cout<<"please enter a valid command"<<std::endl;
+        }
+    }
     // break loop at "exit" keyword
     // process arguments + prompt if needed (i.e."add", please input name "Name Lastname")
-    // switch statement
+*/
+}
 
+int init_file(std::string filename, std::fstream* file){
+    std::cout<<"filename is: " << filename <<"*" <<std::endl;
+
+    if (!filename.length())
+        return 0; //whack rn but it's okay
+
+    std::fstream* temp = new std::fstream();
+    temp->open(filename, std::fstream::in | std::fstream::out);
+    if(*temp) {
+        std::cout<<"file exists";
+        file = temp;
+    } else {
+        std::cout<<"file creation initiated";
+        std::fstream* new_f= new std::fstream;
+        new_f->open(filename, std::fstream::out);
+        // if (!*new_f) {
+        //     std::cout<<"error in file creation"<<std::endl;
+        //     return 0;
+        // }
+        file = new_f;
+        new_f->close();
+    }
+    temp->close();
+    return 1;
 }
