29e1ab7513efa10b718a756fecc270f27383399e
Ruining D <ruiningd@ruinings-macbook-air.home>
2022-09-07 17:07:09 +0200

    finished load_file function, reworked general formats
    Why:
    - the program should be able to read in a scoreboard and rubric file instead of always needing to start from a blank slate
    
    Change in design choices:
    - sets are not as flexible as 2D vectors for our purposes
    - renamed some functions to be less restrictive and more universal

--- score.cpp
+++ score.cpp
@@ -1,102 +1,144 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
-#include <map>
+#include <vector>
+#include <sstream>
 
-int init_file(std::string filename, std::fstream* file);
-int init_scbd(std::fstream* file, std::map<std::string,int>* scbd) {return 1;};
+using namespace std;
+
+std::fstream* init_file(std::string filename);
+int load_file(std::fstream* file, std::vector<vector<string> >* container);
+void print_table(std::vector<vector<string> > table);
 void edit_ptcp() {};
 void edit_pts() {};
-void print_lboard() {};
 void save() {};
+
+int load_file(std::fstream* file, std::vector<vector<string> >* container) {
+    //std::vector<vector<string> > content;
+    std::vector<string> row;
+    std::string line, word;
+    
+    if (!file){
+        cout<<"load_file file segfault"<<endl;
+        return 0;
+        }
+    if (!container){
+        cout<<"load_file container segfault "<<endl;
+        return 0;
+        }
+
+    //file->open();
+    if(file->is_open()) {
+        while(getline(*file, line)) {
+            row.clear();
+            std::stringstream str(line);
+    
+            while(getline(str, word, ','))
+                row.push_back(word);
+            container->push_back(row);
+        }
+    }
+    else{
+        std::cout<<"Could not open the file\n";
+        return 0;
+    }
+
+    return 1;
+    }
+
+    void print_table(std::vector<vector<string> > table){
+        for(int i=0;i<table.size();i++) {
+        for(int j=0;j<table[i].size();j++){
+            std::cout<<table[i][j]<<" ";
+        }
+        std::cout<<"\n";
+        }
+    }
+
 
 int main(){
     //init vars
-    std::string f_name; //filename
+    std::string sf_name; //scoreboard filename
     std::fstream* score_file; //scoreboard file
-    std::map<std::string,int> scbd; //real time scoreboard data structure
+    std::vector<vector<string> > scbd; //real time scoreboard data structure
+
+    //rubric is optional
+    std::string rf_name;
+    std::fstream* rubric_file; 
+    std::vector<vector<string> > rubric;
 
     // welcome message + list of possible actions
     std::cout << "Welcome to scorekeeper!";
 
-    // require storage file name/creation => load/create
+    // require scoreboard file name/creation
     while(true) {
-        std::cout << "Input storage filename";
-        std::cin>> f_name;
-        if (init_file(f_name, score_file)) 
+        std::cout << "Input storage filename: ";
+        std::cin>> sf_name;
+
+        score_file = init_file(sf_name); 
+        if (score_file)
             break;
         std::cout << "Error. Try again.";
     }
-
-
-    if (!init_scbd(score_file, &scbd)){
+    
+    //load scoreboard file
+    if (!load_file(score_file, &scbd)){
         std::cout<<"score_file load error";
         return 0;
     }
 
-/*
     // give rundown of possible actions
-    std::cout<<"Possible actions: participant+/-, points+/-, print, save";
+    std::cout<<"Possible actions: participant (ptcp)+/-, points (pts)+/-, print, save"<<endl;
 
     // for loop to process argument, read until endl
     std::string line;
     while (std::getline(std::cin, line))
     {
-        std::cout << line << std::endl;
-
+        
+        if (!line.length()) continue;
         if(line=="exit") break;
 
         char action ='o';
-        if(line.find("ptcp")==0) action='a';
-        else if(line.find("pts")==0) action='t';
-        else if(line.find("print")==0) action='r';
-        else if(line.find("save")==0) action='s';
-        else action='x';
-
-        switch(action){
-            case 'a':
-                edit_ptcp();
-                break;
-            case 't':
-                edit_pts();
-                break;
-            case 'r':
-                print_lboard();
-                break;
-            case 's':
-                save();
-                break;
-            default:    
-                std::cout<<"please enter a valid command"<<std::endl;
-        }
+        if(line.find("ptcp")==0) edit_ptcp();
+        else if(line.find("pts")==0) edit_pts();
+        else if(line.find("print")==0) print_table(scbd);
+        else if(line.find("save")==0) save();
+        else std::cout<<"please enter a valid command"<<std::endl;
     }
-    // break loop at "exit" keyword
     // process arguments + prompt if needed (i.e."add", please input name "Name Lastname")
-*/
+
+    score_file->close();
+    //rubric_file->close();
+    delete score_file;
+    delete rubric_file;
 }
 
-int init_file(std::string filename, std::fstream* file){
+std::fstream* init_file(std::string filename){
     std::cout<<"filename is: " << filename <<"*" <<std::endl;
 
     if (!filename.length())
-        return 0; //whack rn but it's okay
+        return nullptr; //whack rn but it's okay
 
     std::fstream* temp = new std::fstream();
+    //file = temp;
     temp->open(filename, std::fstream::in | std::fstream::out);
     if(*temp) {
-        std::cout<<"file exists";
-        file = temp;
+        std::cout<<"file exists"<<endl;
+        //cout<<"temp is"<<temp <<endl;
+        //return temp;
     } else {
-        std::cout<<"file creation initiated";
+        std::cout<<"file creation initiated"<<endl;
         std::fstream* new_f= new std::fstream;
         new_f->open(filename, std::fstream::out);
         // if (!*new_f) {
         //     std::cout<<"error in file creation"<<std::endl;
         //     return 0;
         // }
-        file = new_f;
-        new_f->close();
+        //file = new_f;
+        //new_f->close();
+        temp->close();
+        return new_f;
     }
-    temp->close();
-    return 1;
+    //temp->close();
+    return temp;
 }
