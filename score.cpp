4bc22fdf0f6bf9eb738e654f53b5ed486c473310
Ruining D <ruiningd@ruinings-macbook-air.home>
2022-09-10 01:00:53 +0200

    completed edit_table, save_file and tested print_table
    purpose:
    - the progam is now able to complete all basic functions regarding the scoreboard file and table
    - generalised functions can easily be adapted to suit rubric operations as well
    - edit_table enables changes to be made tot he scoreboard such as changing participant points values and adding players
    
    change in design choices:
    - edit_table now takes the place of 2 separate functions: adding a new player and changing the point value of existing players
    - filestream now does truncation when overwriting
    - point values stored as strings for easier file operations and only converted to int for point changes
    
    testing:
    - complete with checking save does write to existing and new files in each case
    - adding new players, adding points to old players, removing points to old players

--- score.cpp
+++ score.cpp
@@ -9,52 +9,8 @@ using namespace std;
 std::fstream* init_file(std::string filename);
 int load_file(std::fstream* file, std::vector<vector<string> >* container);
 void print_table(std::vector<vector<string> > table);
-void edit_ptcp() {};
-void edit_pts() {};
-void save() {};
-
-int load_file(std::fstream* file, std::vector<vector<string> >* container) {
-    //std::vector<vector<string> > content;
-    std::vector<string> row;
-    std::string line, word;
-    
-    if (!file){
-        cout<<"load_file file segfault"<<endl;
-        return 0;
-        }
-    if (!container){
-        cout<<"load_file container segfault "<<endl;
-        return 0;
-        }
-
-    //file->open();
-    if(file->is_open()) {
-        while(getline(*file, line)) {
-            row.clear();
-            std::stringstream str(line);
-    
-            while(getline(str, word, ','))
-                row.push_back(word);
-            container->push_back(row);
-        }
-    }
-    else{
-        std::cout<<"Could not open the file\n";
-        return 0;
-    }
-
-    return 1;
-    }
-
-    void print_table(std::vector<vector<string> > table){
-        for(int i=0;i<table.size();i++) {
-        for(int j=0;j<table[i].size();j++){
-            std::cout<<table[i][j]<<" ";
-        }
-        std::cout<<"\n";
-        }
-    }
-
+void edit_table(std::vector<vector<string> >* container, std::string input);
+void save_file(std::fstream* file, std::string filename, std::vector<vector<string> > table);
 
 int main(){
     //init vars
@@ -80,7 +36,7 @@ int main(){
             break;
         std::cout << "Error. Try again.";
     }
-    
+
     //load scoreboard file
     if (!load_file(score_file, &scbd)){
         std::cout<<"score_file load error";
@@ -88,7 +44,13 @@ int main(){
     }
 
     // give rundown of possible actions
-    std::cout<<"Possible actions: participant (ptcp)+/-, points (pts)+/-, print, save"<<endl;
+    std::cout<<"Possible actions: 
+        edit scoreboard (scbd), 
+        edit rubric (rubric), 
+        print, 
+        save"<<endl;
+    //add rubric print n save
+    //add option to repeat action list
 
     // for loop to process argument, read until endl
     std::string line;
@@ -99,16 +61,16 @@ int main(){
         if(line=="exit") break;
 
         char action ='o';
-        if(line.find("ptcp")==0) edit_ptcp();
-        else if(line.find("pts")==0) edit_pts();
+        if(line.find("scbd")==0) edit_table(&scbd, line.substr(5));
+        else if(line.find("rubric")==0) edit_table(&rubric, line.substr(7));
         else if(line.find("print")==0) print_table(scbd);
-        else if(line.find("save")==0) save();
+        else if(line.find("save")==0) save_file(score_file, sf_name, scbd);
         else std::cout<<"please enter a valid command"<<std::endl;
     }
     // process arguments + prompt if needed (i.e."add", please input name "Name Lastname")
 
     score_file->close();
-    //rubric_file->close();
+    if (rubric_file!=nullptr) rubric_file->close();
     delete score_file;
     delete rubric_file;
 }
@@ -117,28 +79,116 @@ std::fstream* init_file(std::string filename){
     std::cout<<"filename is: " << filename <<"*" <<std::endl;
 
     if (!filename.length())
-        return nullptr; //whack rn but it's okay
+        return nullptr;
 
     std::fstream* temp = new std::fstream();
-    //file = temp;
-    temp->open(filename, std::fstream::in | std::fstream::out);
+    temp->open(filename, std::fstream::in);
     if(*temp) {
         std::cout<<"file exists"<<endl;
-        //cout<<"temp is"<<temp <<endl;
-        //return temp;
     } else {
         std::cout<<"file creation initiated"<<endl;
         std::fstream* new_f= new std::fstream;
         new_f->open(filename, std::fstream::out);
-        // if (!*new_f) {
-        //     std::cout<<"error in file creation"<<std::endl;
-        //     return 0;
-        // }
-        //file = new_f;
-        //new_f->close();
         temp->close();
         return new_f;
     }
-    //temp->close();
     return temp;
+}
+
+
+int load_file(std::fstream* file, std::vector<vector<string> >* container) {
+    std::vector<string> row;
+    std::string line, word;
+    
+    if (!file){
+        cout<<"load_file file segfault"<<endl;
+        return 0;
+        }
+    if (!container){
+        cout<<"load_file container segfault "<<endl;
+        return 0;
+        }
+
+    if(file->is_open()) {
+        while(getline(*file, line)) {
+            row.clear();
+            std::stringstream str(line);
+    
+            while(getline(str, word, ','))
+                row.push_back(word);
+            container->push_back(row);
+        }
+    }
+    else{
+        std::cout<<"Could not open the file\n";
+        return 0;
+    }
+
+    return 1;
+}
+
+void print_table(std::vector<vector<string> > table){
+    for(int i=0;i<table.size();i++) {
+    for(int j=0;j<table[i].size();j++){
+        std::cout<<table[i][j]<<" ";
+    }
+    std::cout<<"\n";
+    }
+}
+
+void edit_table(std::vector<vector<string> >* container, std::string input) {
+    //parse input
+    std::size_t found_num = input.find_first_of(",")+1;
+    if (found_num == std::string::npos){
+        cout<<"no number found, input error. Please fix and try again."<<endl;
+        return;
+    }
+
+    int len = input.length();
+    std::string name = input.substr(0, found_num-1);
+    if (!name.length()){
+        cout<<"empty name, input error"<<endl;
+        return;
+    }
+
+    input = input.substr(found_num);
+    std::size_t found_space = input.find_first_of(" ");
+    std::string points;
+    if (found_space == std::string::npos){
+        points = input;
+    }
+    else{
+        len = input.length();
+        points = input.substr(0, len-found_space);
+    }
+    int pts = std::stoi(points);
+
+    //if name already in table, edit points (convert, add, convert back)
+    bool existing = false;
+
+    for (auto i = container->begin(); i != container->end(); ++i) {
+        string to_comp =*(i->begin());
+        if (to_comp==name){
+            existing = true;
+            string old_points = *(i->rbegin());
+            *(i->rbegin()) = to_string(stoi(old_points)+pts);
+        }
+    }
+
+    //else, add new entry (convert to string)
+    if (!existing){
+        vector<string> row;
+        row.push_back(name);
+        row.push_back(points);
+        container->push_back(row);
+    }
+}
+
+void save_file(std::fstream* file, std::string filename, std::vector<vector<string> > table){
+    file->close();
+    file->open(filename, std::ofstream::out | std::ofstream::trunc);
+
+    for(int i=0;i<table.size();i++) {
+        *file<<table[i][0]<<","<<table[i][1]<<"\n";
+    }
 }
